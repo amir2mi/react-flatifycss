186713ec09a29f49e1e5fd570bffe1dd2d0c70a6
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-01-18 00:39:54 +0330

    Converted enums to union type for general props interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,57 +1,45 @@
-enum Colors {
-  'default' = 'default',
-  'blue' = 'blue',
-  'blue-light' = 'blue-light',
-  'green' = 'green',
-  'green-light' = 'green-light',
-  'yellow' = 'yellow',
-  'yellow-light' = 'yellow-light',
-  'orange' = 'orange',
-  'orange-light' = 'orange-light',
-  'red' = 'red',
-  'red-light' = 'red-light',
-  'pink' = 'pink',
-  'pink-light' = 'pink-light',
-  'purple' = 'purple',
-  'purple-light' = 'purple-light',
-  'accent' = 'accent',
-  'accent-light' = 'accent-light',
-  'success' = 'success',
-  'success-light' = 'success-light',
-  'info' = 'info',
-  'info-light' = 'info-light',
-  'warning' = 'warning',
-  'warning-light' = 'warning-light',
-  'danger' = 'danger',
-  'danger-light' = 'danger-light',
-  'light' = 'light',
-  'light-light' = 'light-light',
-  'dark' = 'dark',
-  'dark-light' = 'dark-light',
-}
+type Colors =
+  | 'default'
+  | 'blue'
+  | 'blue-light'
+  | 'green'
+  | 'green-light'
+  | 'yellow'
+  | 'yellow-light'
+  | 'orange'
+  | 'orange-light'
+  | 'red'
+  | 'red-light'
+  | 'pink'
+  | 'pink-light'
+  | 'purple'
+  | 'purple-light'
+  | 'accent'
+  | 'accent-light'
+  | 'success'
+  | 'success-light'
+  | 'info'
+  | 'info-light'
+  | 'warning'
+  | 'warning-light'
+  | 'danger'
+  | 'danger-light'
+  | 'light'
+  | 'light-light'
+  | 'dark'
+  | 'dark-light';
 
-enum Roundness {
-  'default' = 'default',
-  'sharp' = 'sharp',
-  'round-sm' = 'round-sm',
-  'round-md' = 'round-md',
-  'round-lg' = 'round-lg',
-  'circle' = 'circle',
-  'oval' = 'oval',
-  'triangle' = 'triangle',
-}
+type Roundness =
+  | 'default'
+  | 'sharp'
+  | 'round-sm'
+  | 'round-md'
+  | 'round-lg'
+  | 'circle'
+  | 'oval'
+  | 'triangle';
 
-enum Sizes {
-  'default' = 'default',
-  'xs' = 'xs',
-  'sm' = 'sm',
-  'md' = 'md',
-  'lg' = 'lg',
-  '2x' = '2x',
-  '3x' = '3x',
-  '4x' = '4x',
-  '5x' = '5x',
-}
+type Sizes = 'default' | 'xs' | 'sm' | 'md' | 'lg' | '2x' | '3x' | '4x' | '5x';
 
 export interface FlatifyGeneralProps {
   style?: Colors;
