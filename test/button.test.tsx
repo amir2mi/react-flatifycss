4fda24dbaa60e5165a747e539dce96e3e87afe9b
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-01-18 19:34:57 +0330

    Added React testing library

--- test/button.test.tsx
+++ test/button.test.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from 'react-dom';
 import { Default as Button } from '../stories/button.stories';
 
 describe('Button', () => {
-  it('renders without crashing', () => {
+  it('should be rendered without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Button />, div);
     ReactDOM.unmountComponentAtNode(div);
