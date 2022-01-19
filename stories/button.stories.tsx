8e3e7c2560893d8167ac918f61af06ae4138fc91
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-01-19 13:46:53 +0330

    Storybook - Added button with icon as secondaryText

--- stories/button.stories.tsx
+++ stories/button.stories.tsx
@@ -21,6 +21,7 @@ export const Primary = Template.bind({});
 export const Secondary = Template.bind({});
 export const Tertiary = Template.bind({});
 export const TwoLayer = Template.bind({});
+export const TwoLayerIcon = Template.bind({});
 
 Default.args = {
   text: 'Hello ',
@@ -49,4 +50,13 @@ Tertiary.args = {
 TwoLayer.args = {
   ...Default.args,
   secondaryText: 'Secondary',
+};
+
+TwoLayerIcon.args = {
+  ...Default.args,
+  secondaryText: (
+    <svg xmlns="http://www.w3.org/2000/svg" x="0" y="0" viewBox="0 0 16 16">
+      <path d="M3.3 12.7c.2.2.4.3.7.3s.5-.1.7-.3L8 9.4l3.3 3.3c.2.2.5.3.7.3s.5-.1.7-.3c.4-.4.4-1 0-1.4L9.4 8l3.3-3.3c.4-.4.4-1 0-1.4s-1-.4-1.4 0L8 6.6 4.7 3.3c-.4-.4-1-.4-1.4 0s-.4 1 0 1.4L6.6 8l-3.3 3.3c-.4.4-.4 1 0 1.4z"></path>
+    </svg>
+  ),
 };
