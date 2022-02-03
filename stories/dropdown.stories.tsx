85c58d637a93cf456f62191b5f4f540bae4e88ac
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-02-04 00:20:32 +0330

    Dropdown - Improved storybook

--- stories/dropdown.stories.tsx
+++ stories/dropdown.stories.tsx
@@ -15,16 +15,41 @@ export default meta;
 const Template: Story = (args) => <Dropdown {...args} />;
 
 export const Default = Template.bind({});
-export const Expendable = Template.bind({});
-export const Bordered = Template.bind({});
-export const Customized = Template.bind({});
+export const MenuItems = Template.bind({});
 
 Default.args = {
-  autoClose: true
-buttonArrow:
-buttonLabel:
-children:
-isMenu:
-placement:
-tagName:
+  autoClose: true,
+  buttonArrow: true,
+  buttonLabel: 'Click me',
+  children: 'Im a dropdown',
+  isMenu: false,
+};
+
+MenuItems.args = {
+  autoClose: true,
+  buttonArrow: true,
+  buttonLabel: 'Click me',
+  children: (
+    <>
+      <li className="menu-item heading">My Account</li>
+      <li className="menu-item">
+        <a href="#">Sign up</a>
+      </li>
+      <li className="menu-item">
+        <a href="#">Login</a>
+      </li>
+      <li role="separator" className="menu-item separator"></li>
+      <li className="menu-item">
+        <a href="#">About us</a>
+      </li>
+      <li className="menu-item">
+        <a href="#">Contribute</a>
+      </li>
+      <li className="menu-item">
+        <a href="#">Rate it!</a>
+      </li>
+    </>
+  ),
+  isMenu: true,
+  placement: 'left',
 };
