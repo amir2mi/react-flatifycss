59a09f3b068366c6b744c6a50dc93807958546fa
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-08-09 23:52:30 +0430

    Added dropdown new stories and edited its tests

--- src/dropdown/dropdown.test.tsx
+++ src/dropdown/dropdown.test.tsx
@@ -1,22 +1,47 @@
 import React from 'react';
 import { render, screen, fireEvent, waitFor } from '@testing-library/react';
-import { Dropdown } from '../dropdown';
+import { Dropdown, DropdownButton, DropdownBody } from '../dropdown';
+
+const TestDropdown = () => (
+  <Dropdown id="test-dropdown" data-testid="dropdown">
+    <DropdownButton theme="purple-light" hasArrow>
+      Open Menu
+    </DropdownButton>
+    <DropdownBody isMenu theme="dark">
+      <li className="menu-item heading">My Account</li>
+      <li className="menu-item">
+        <a href="#">Sign up</a>
+      </li>
+      <li className="menu-item">
+        <a href="#">Login</a>
+      </li>
+      <li role="separator" className="menu-item separator"></li>
+      <li className="menu-item">
+        <a href="#">About us</a>
+      </li>
+      <li className="menu-item">
+        <a href="#">Contribute</a>
+      </li>
+      <li className="menu-item">
+        <a href="#">Rate it!</a>
+      </li>
+    </DropdownBody>
+  </Dropdown>
+);
 
 describe('Dropdown', () => {
-  it('should be rendered without crashing', async () => {
-    render(<Dropdown id="test-dropdown" data-testid="dropdown" />);
+  it('should be rendered without crashing', () => {
+    render(<TestDropdown />);
 
-    await waitFor(() => {
+    waitFor(() => {
       expect(screen.getByTestId('dropdown')).toBeInTheDocument();
     });
   });
 
-  it('should be opened on click', async () => {
-    const { container } = render(
-      <Dropdown id="test-dropdown" data-testid="dropdown" />
-    );
+  it('should be opened on click', () => {
+    const { container } = render(<TestDropdown />);
 
-    await waitFor(() => {
+    waitFor(() => {
       expect(screen.getByTestId('dropdown')).toBeInTheDocument();
     });
 
--- stories/dropdown.stories.tsx
+++ stories/dropdown.stories.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { Dropdown } from '../src';
-import DropdownButton from '../src/dropdown/dropdown-button';
-import DropdownBody from '../src/dropdown/dropdown-body';
+import { Dropdown, DropdownButton, DropdownBody } from '../src';
 
 const meta: Meta = {
   title: 'Components/Dropdown',
@@ -14,9 +12,18 @@ const meta: Meta = {
 
 export default meta;
 
-const Template: Story = (args) => (
+const Simple: Story = (args) => (
   <Dropdown id="test-dropdown" {...args}>
-    <DropdownButton arrow>Open</DropdownButton>
+    <DropdownButton>Open it!</DropdownButton>
+    <DropdownBody>
+      <h6>Gotcha!</h6>
+    </DropdownBody>
+  </Dropdown>
+);
+
+const Menu: Story = (args) => (
+  <Dropdown id="test-dropdown" {...args}>
+    <DropdownButton hasArrow>Open Menu</DropdownButton>
     <DropdownBody isMenu>
       <li className="menu-item heading">My Account</li>
       <li className="menu-item">
@@ -39,37 +46,58 @@ const Template: Story = (args) => (
   </Dropdown>
 );
 
-export const Default = Template.bind({});
-export const MenuItems = Template.bind({});
-export const CustomLabel = Template.bind({});
-export const CustomOffset = Template.bind({});
+const Custom: Story = (args) => (
+  <Dropdown id="test-dropdown" {...args}>
+    <DropdownButton theme="purple-light" hasArrow>
+      Open Menu
+    </DropdownButton>
+    <DropdownBody isMenu theme="dark">
+      <li className="menu-item heading">My Account</li>
+      <li className="menu-item">
+        <a href="#">Sign up</a>
+      </li>
+      <li className="menu-item">
+        <a href="#">Login</a>
+      </li>
+      <li role="separator" className="menu-item separator"></li>
+      <li className="menu-item">
+        <a href="#">About us</a>
+      </li>
+      <li className="menu-item">
+        <a href="#">Contribute</a>
+      </li>
+      <li className="menu-item">
+        <a href="#">Rate it!</a>
+      </li>
+    </DropdownBody>
+  </Dropdown>
+);
+
+export const Default = Simple.bind({});
+export const MenuItems = Menu.bind({});
+export const Placement = Menu.bind({});
+export const AutoClose = Menu.bind({});
+export const Offset = Menu.bind({});
+export const Customized = Custom.bind({});
 
 Default.args = {
-  autoClose: true,
-  buttonArrow: true,
-  buttonLabel: 'Click me',
   children: 'Im a dropdown',
   isMenu: false,
-};
-
-MenuItems.args = {
   autoClose: true,
-  buttonArrow: true,
-  buttonLabel: 'Click me',
-  isMenu: true,
-  placement: 'left',
 };
 
-CustomLabel.args = {
+MenuItems.args = { autoClose: true };
+
+Placement.args = {
+  placement: 'right',
   autoClose: true,
-  buttonArrow: true,
-  buttonLabel: <i>I am a JSX element</i>,
-  children: 'Im a dropdown',
-  isMenu: false,
 };
 
-CustomOffset.args = {
-  ...Default.args,
-  offsetX: 100,
-  offsetY: 0,
+AutoClose.args = {
+  autoClose: 'inside',
+};
+
+Offset.args = {
+  offsetX: 120,
+  offsetY: -10,
 };
