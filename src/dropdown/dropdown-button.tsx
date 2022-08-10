0968a7b9285474ca5c8eeb2cb8c7b468c98ef1dd
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-08-10 23:33:17 +0430

    Added mouse events to dropdown button

--- src/dropdown/dropdown-button.tsx
+++ src/dropdown/dropdown-button.tsx
@@ -8,12 +8,24 @@ interface DropdownButtonProps extends FlatifyGeneralProps {
   children?: React.ReactNode;
   hasArrow?: boolean;
   innerRef?: React.Ref<HTMLButtonElement>;
+  isButton?: boolean;
   isOpen?: boolean;
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  onMouseEnter?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  onMouseLeave?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
 export default function DropdownButton(props: DropdownButtonProps) {
-  const { hasArrow, children, isOpen, onClick, innerRef } = props;
+  const {
+    hasArrow,
+    children,
+    isOpen,
+    isButton,
+    onClick,
+    onMouseEnter,
+    onMouseLeave,
+    innerRef,
+  } = props;
 
   return (
     <button
@@ -21,13 +33,16 @@ export default function DropdownButton(props: DropdownButtonProps) {
       ref={innerRef}
       aria-expanded={isOpen}
       className={clsx(
-        'button dropdown-toggle',
+        'dropdown-toggle',
+        isButton !== false && 'button',
         hasArrow && 'arrow-button',
         isOpen && 'active',
-        isOpen && 'arrow-flip',
+        isOpen && hasArrow && 'arrow-flip',
         ...generalClasses(props)
       )}
       onClick={onClick}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
     >
       {children}
     </button>
