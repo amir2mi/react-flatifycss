f2f8aa10083bcddf492b4943878295bbbfa68237
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-08-19 23:16:15 +0430

    Added progress component stories

---
+++ stories/progress.stories.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { Meta, Story } from '@storybook/react';
+import { Progress } from '../src';
+
+const meta: Meta = {
+  title: 'Components/Progress',
+  component: Progress,
+  parameters: {
+    controls: { expanded: true },
+  },
+};
+
+export default meta;
+
+const Template: Story = args => {
+  return <Progress {...args} value={args.value} />;
+};
+
+export const Default = Template.bind({});
+export const Bordered = Template.bind({});
+export const Processing = Template.bind({});
+export const Customized = Template.bind({});
+
+Default.args = {
+  value: 50,
+};
+
+Bordered.args = {
+  value: 50,
+  bordered: true,
+};
+
+Processing.args = {
+  value: 50,
+  processing: true,
+};
+
+Customized.args = {
+  value: 15,  
+  bordered: true,
+  processing: true,
+  theme: 'green-light',
+};
