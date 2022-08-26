357aa962cf3664353279ee8ee8857e6abb44268f
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-08-26 17:55:44 +0430

    Fixed dropdown body general attributes issue not being called

--- src/dropdown/dropdown-body.tsx
+++ src/dropdown/dropdown-body.tsx
@@ -52,7 +52,7 @@ export default function DropdownBody(props: DropdownBodyProps) {
     >
       <DropdownBodyElement
         {...rest}
-        {...generalAttributes}
+        {...generalAttributes(props)}
         ref={innerRef}
         style={style}
         className={clsx(
