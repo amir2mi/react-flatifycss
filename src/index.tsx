2b1ba321f110ee270f3101fdadb51e44a27cbc29
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-08-28 01:06:06 +0430

    Removed close button component path from the main index file

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ export * from './button-group';
 export * from './add-button';
 export * from './arrow-button';
 export * from './hamburger';
-export * from './close-button';
 
 // form
 export * from './input';
