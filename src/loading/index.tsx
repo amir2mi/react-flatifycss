8deb6a56779f3a0d07f253583c962fe9fb9de1e1
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-09-01 01:12:37 +0430

    Improved loading component and added spinner with dynamic styles and stories

--- src/loading/index.tsx
+++ src/loading/index.tsx
@@ -1,25 +1,48 @@
 import React from 'react';
 import clsx from 'clsx';
+import styled from 'styled-components';
 import { FlatifyGeneralProps } from '../interfaces';
 import { generalClasses } from '../classes';
 
-interface LoadingProps extends FlatifyGeneralProps {
-  isStopped?: boolean;
+interface LoadingProps
+  extends FlatifyGeneralProps,
+    Omit<React.HTMLAttributes<HTMLElement>, 'color'> {
+  dotWidth?: string;
+  dotHeight?: string;
+  dotStartScale?: number;
+  duration?: number;
+  spinner?: boolean;
+  stopped?: boolean;
 }
 
+const LoadingWrapper = styled.span`
+  ${({ dotWidth }: LoadingProps) =>
+    dotWidth ? `--flatify__loading-dot-width: ${dotWidth};` : ''}
+  ${({ dotHeight }: LoadingProps) =>
+    dotHeight ? `--flatify__loading-dot-height: ${dotHeight};` : ''} 
+  ${({ dotStartScale }: LoadingProps) =>
+    dotStartScale
+      ? `--flatify__loading-dot-animation-start-scale: ${dotStartScale};`
+      : ''}
+  ${({ duration }: LoadingProps) =>
+    duration
+      ? `--flatify__loading-dot-animation-duration: ${duration}ms; --flatify__loading-spinner-animation-duration: ${duration}ms;`
+      : ''}
+  ${({ sx }: LoadingProps) => (sx ? sx : '')}
+`;
+
 export function Loading(props: LoadingProps) {
-  const { isStopped } = props;
+  const { spinner, stopped, ...rest } = props;
 
   return (
-    <span
-      className={clsx(
-        'loading',
-        {
-          'stop-animation': isStopped,
-        },
-        ...generalClasses(props)
-      )}
+    <LoadingWrapper
+      {...rest}
       aria-hidden
-    ></span>
+      className={clsx(...generalClasses(props), {
+        loading: !spinner,
+        spinner: spinner,
+        'stop-animation': stopped,
+      })}
+    />
   );
 }
--- stories/loading.stories.tsx
+++ stories/loading.stories.tsx
@@ -12,11 +12,31 @@ const meta: Meta = {
 
 export default meta;
 
-const Template: Story = (args) => <Loading {...args} />;
+const Template: Story = args => <Loading {...args} />;
 
 export const Default = Template.bind({});
+export const Spinner = Template.bind({});
+export const Duration = Template.bind({});
 export const Stopped = Template.bind({});
+export const Customized = Template.bind({});
 
-Stopped.args={
-  isStopped: true,
-}
+Stopped.args = {
+  stopped: true,
+};
+
+Spinner.args = {
+  spinner: true,
+};
+
+Duration.args = {
+  spinner: true,
+  duration: 5000,
+};
+
+Customized.args = {
+  duration: 750,
+  dotWidth: '0.5em',
+  dotHeight: '1em',
+  color: 'orange',
+  dotStartScale: 0.25,
+};
