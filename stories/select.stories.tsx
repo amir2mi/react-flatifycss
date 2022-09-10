1b623cb0c6bb016a096dcd46318d11684507de3c
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-09-11 01:24:18 +0430

    Updated select component stories

--- stories/select.stories.tsx
+++ stories/select.stories.tsx
@@ -12,26 +12,73 @@ const meta: Meta = {
 
 export default meta;
 
-const items = [
+const itemsDefault = [
   { label: 'Planes', value: 'planes' },
   { label: 'Cars', value: 'cars' },
   { label: 'Trains', value: 'trains' },
-  { label: 'Bikes', value: 'bikes' },
+  { label: 'Scooter', value: 'scooter' },
+  { label: 'Skates', value: 'skates' },
+  { label: 'Skateboards', value: 'skateboards' },
+  { label: 'Walking', value: 'walking' },
+  { label: 'Talking', value: 'talking' },
 ];
 
-const Template: Story = (args) => (
-  <Select id="storybook-select" items={items} {...args} />
+const itemsGrouped = [
+  {
+    label: 'Motorized vehicles',
+    options: [
+      { label: 'Planes', value: 'planes' },
+      { label: 'Cars', value: 'cars' },
+      { label: 'Trains', value: 'trains' },
+    ],
+  },
+  {
+    label: 'Non-motorized vehicles',
+    options: [
+      { label: 'Scooter', value: 'scooter' },
+      { label: 'Skates', value: 'skates' },
+      { label: 'Skateboards', value: 'skateboards' },
+    ],
+  },
+  { label: 'Walking', value: 'walking' },
+  { label: 'Talking', value: 'talking' },
+];
+
+const Template: Story = args => (
+  <Select id="storybook-select" options={itemsGrouped} {...args} />
 );
 
 export const Default = Template.bind({});
+export const Grouped = Template.bind({});
+export const Controlled = Template.bind({});
 export const Multiple = Template.bind({});
 
 Default.args = {
-  label:"Choose one item:",
-  onChange: (value) => console.log(value),
+  label: 'Choose one item:',
+  defaultValue: 'trains',
+  options: itemsDefault,
+  onChange: value => alert('Selected option is => ' + value),
+};
+
+Grouped.args = {
+  label: 'Choose one item:',
+  onChange: value => alert('Selected option is => ' + value),
+};
+Controlled.args = {
+  label: 'Controlled Select (should set the value manually):',
+  value: 'bikes',
+  onChange: value => alert('Selected option is => ' + value),
 };
 
 Multiple.args = {
+  label: 'Choose as much as you can:',
   multiple: true,
-  onChange: (value) => console.log(value),
+  defaultValue: ['bikes', 'trains'],
+  onChange: value => {
+    if (Array.isArray(value) && value.length > 1) {
+      alert('Selected options are => ' + value);
+    } else {
+      alert('Selected option is => ' + value);
+    }
+  },
 };
