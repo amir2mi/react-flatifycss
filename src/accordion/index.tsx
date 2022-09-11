35a3bb3f7d628c02b1918d3b7b1399d51d756285
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-09-11 16:36:40 +0430

    Added ReachUI credit to accordion index file

--- src/accordion/index.tsx
+++ src/accordion/index.tsx
@@ -1,3 +1,7 @@
+/*
+ * Accordion component created with ReachUI library
+ * These components are wrappers to customize the default styles
+ */
 import Accordion from './accordion';
 import AccordionItem from './accordion-item';
 import AccordionButton from './accordion-button';
