1adb02b9c69a00918bbda19b5beded9f02f181ed
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-09-11 18:46:23 +0430

    Exported toggle-switch component interface

--- src/toggle-switch/index.tsx
+++ src/toggle-switch/index.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import { FlatifyGeneralProps } from '../interfaces';
 import { generalClasses } from '../classes';
 
-interface ToggleSwitchProps
+export interface ToggleSwitchProps
   extends FlatifyGeneralProps,
     Omit<React.HTMLAttributes<HTMLInputElement>, 'color' | 'onChange'> {
   checked?: boolean;
