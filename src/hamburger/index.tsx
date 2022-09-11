7033b69ae23d3861fe5c181ea20d4b33425c6bd4
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-09-11 18:24:47 +0430

    Exported hamburger component interface

--- src/hamburger/index.tsx
+++ src/hamburger/index.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import { FlatifyGeneralProps } from '../interfaces';
 import { generalClasses } from '../classes';
 
-interface HamburgerProps
+export interface HamburgerProps
   extends FlatifyGeneralProps,
     Omit<React.HTMLAttributes<HTMLElement>, 'color'> {
   active: boolean;
