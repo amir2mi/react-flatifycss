c0519b45833bdd69c2e78c29133899380f445685
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-09-11 18:27:56 +0430

    Exported radio component interface

--- src/radio/index.tsx
+++ src/radio/index.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import { FlatifyGeneralProps } from '../interfaces';
 import { generalClasses } from '../classes';
 
-interface RadioProps
+export interface RadioProps
   extends FlatifyGeneralProps,
     Omit<React.HTMLAttributes<HTMLInputElement>, 'color' | 'onChange'> {
   checked?: boolean;
