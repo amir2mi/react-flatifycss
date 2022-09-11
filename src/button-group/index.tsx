3e0677115408925307a9c3b562ef3ecbb3c25937
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-09-11 18:22:00 +0430

    Exported button-group component interface

--- src/button-group/index.tsx
+++ src/button-group/index.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import { FlatifyGeneralProps } from '../interfaces';
 import { generalClasses } from '../classes';
 
-interface ButtonGroupProps
+export interface ButtonGroupProps
   extends FlatifyGeneralProps,
     Omit<React.HTMLAttributes<HTMLElement>, 'color'> {
   children?: React.ReactNode;
