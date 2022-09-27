a55fb17c3cbaa13c6662a2b75e7f2bc1715ede29
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-09-28 00:58:03 +0330

    Moved toasts wrapper elements into body

--- src/toast/toasts-wrapper.tsx
+++ src/toast/toasts-wrapper.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 export default function ToastsWrapper() {
-  return (
+  return ReactDOM.createPortal(
     <>
-      <div className="toast-wrapper right bottom" />
       <div className="toast-wrapper right top" />
-      <div className="toast-wrapper left bottom" />
-      <div className="toast-wrapper left top" />
-      <div className="toast-wrapper center bottom" />
+      <div className="toast-wrapper right bottom" />
       <div className="toast-wrapper center top" />
-    </>
+      <div className="toast-wrapper center bottom" />
+      <div className="toast-wrapper left top" />
+      <div className="toast-wrapper left bottom" />
+    </>,
+    document.body
   );
 }
