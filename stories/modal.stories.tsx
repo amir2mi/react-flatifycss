abee9ee8237c8e8311f722328d0ba8d2c81bdde6
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-10-07 17:22:55 +0330

    Added modal doc page and new custom overlay story

--- stories/modal.stories.tsx
+++ stories/modal.stories.tsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import { Meta, Story } from '@storybook/react';
 import { Modal, Button, ModalHeader, ModalBody, ModalFooter } from '../src';
+import ModalPage from './modal.mdx';
 
 const meta: Meta = {
   title: 'Components/Modal',
   component: Modal,
   parameters: {
     controls: { expanded: true },
+    docs: {
+      page: ModalPage,
+    },
   },
 };
 
@@ -57,14 +61,66 @@ const Template: Story = args => {
   );
 };
 
+const TemplateWithInlineStyle: Story = args => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <style>
+        {`.custom-modal-overlay {
+  opacity: 1 !important;
+  background: rgb(30, 42, 53, 0.35);
+  backdrop-filter: blur(5px) !important;
+}`}
+      </style>
+      <Button onClick={() => setIsOpen(!isOpen)}>Open The Modal</Button>
+      <Modal
+        {...args}
+        overlayClassName={args.overlayClassName}
+        bordered={args.bordered}
+        position={args.position}
+        aria-label="a simple modal"
+        isOpen={isOpen}
+        onDismiss={() => setIsOpen(false)}
+      >
+        <ModalHeader>
+          <h3 className="modal-title">I am a big boy</h3>
+          <button
+            className="close-button close-modal"
+            aria-label="Close the modal"
+            onClick={() => setIsOpen(false)}
+          />
+        </ModalHeader>
+        <ModalBody>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque sed
+          earum repudiandae! Veritatis, quo! Optio distinctio, dolorum eaque
+          facilis neque, temporibus nihil exercitationem commodi amet hic iste
+          reprehenderit voluptas impedit! Lorem ipsum, dolor sit amet
+          consectetur adipisicing elit. Atque sed earum repudiandae! Veritatis,
+          quo! Optio distinctio, dolorum eaque facilis neque, temporibus nihil
+          exercitationem commodi amet hic iste reprehenderit voluptas impedit!
+        </ModalBody>
+        <ModalFooter>
+          <Button theme="danger" onClick={() => setIsOpen(false)}>
+            No, You Are Not!
+          </Button>
+          <Button theme="success" onClick={() => setIsOpen(false)}>
+            Okay
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </>
+  );
+};
+
 export const Default = Template.bind({});
 export const DisabledOverlayClick = Template.bind({});
 export const Bordered = Template.bind({});
 export const Positioned = Template.bind({});
+export const CustomOverlay = TemplateWithInlineStyle.bind({});
 
 Default.args = {
   position: 'center',
-  overlayClassName: 'custom-modal-overlay',
 };
 
 DisabledOverlayClick.args = {
@@ -74,9 +130,12 @@ DisabledOverlayClick.args = {
 Bordered.args = {
   position: 'center',
   bordered: true,
-  overlayClassName: 'custom-modal-overlay',
 };
 
 Positioned.args = {
   position: 'bottom',
+};
+
+CustomOverlay.args = {
+  overlayClassName: 'custom-modal-overlay',
 };
