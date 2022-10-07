beb5bfaa09b2121c7624f6a2cc89f789c6a53131
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-10-07 21:39:30 +0330

    Added tabs doc page and improved stories

--- stories/tabs.stories.tsx
+++ stories/tabs.stories.tsx
@@ -1,75 +1,34 @@
 import { Meta, Story } from '@storybook/react';
 import React from 'react';
 import { Badge, Tab, TabList, TabPanel, TabPanels, Tabs } from '../src';
+import TabsPage from './tabs.mdx';
 
 const meta: Meta = {
   title: 'Components/Tabs',
   component: Tabs,
   parameters: {
     controls: { expanded: true },
+    docs: {
+      page: TabsPage,
+    },
   },
 };
 
 export default meta;
 
-const Template: Story = args => (
-  <Tabs {...args}>
-    <TabList>
-      <Tab>Kafka</Tab>
-      <Tab onClick={() => alert('clicked on Werther')}>Werther</Tab>
-      <Tab>
-        Cicero
-        <Badge>256</Badge>
-      </Tab>
-    </TabList>
-    <TabPanels>
-      <TabPanel>
-        <p>
-          One morning, when Gregor Samsa woke from troubled dreams, he found
-          himself transformed in his bed into a horrible vermin. He lay on his
-          armour-like back, and if he lifted his head a little he could see his
-          brown belly, slightly domed and divided by arches into stiff sections.
-          The bedding was hardly able to cover it and seemed ready to slide off
-          any moment. His many legs, pitifully thin compared with the size of
-          the rest of him, waved about helplessly as he looked. "What is
-          happened to me?" he thought. It was not a dream. His room, a proper
-          human room although a little too small, lay peacefully between its
-          four familiar walls.
-        </p>
-      </TabPanel>
-      <TabPanel>
-        <p>
-          A wonderful serenity has taken possession of my entire soul, like
-          these sweet mornings of spring which I enjoy with my whole heart. I am
-          alone, and feel the charm of existence in this spot, which was created
-          for the bliss of souls like mine. I am so happy, my dear friend, so
-          absorbed in the exquisite sense of mere tranquil existence, that I
-          neglect my talents. I should be incapable of drawing a single stroke
-          at the present moment; and yet I feel that I never was a greater
-          artist than now.
-        </p>
-      </TabPanel>
-      <TabPanel>
-        <p>
-          But I must explain to you how all this mistaken idea of denouncing
-          pleasure and praising pain was born and I will give you a complete
-          account of the system, and expound the actual teachings of the great
-          explorer of the truth, the master-builder of human happiness. No one
-          rejects, dislikes, or avoids pleasure itself, because it is pleasure,
-          but because those who do not know how to pursue pleasure rationally
-          encounter consequences that are extremely painful. Nor again is there
-          anyone who loves or pursues or desires to obtain pain of itself,
-          because it is pain, but because occasionally circumstances occur in
-          which toil and pain can procure him some great pleasure.
-        </p>
-      </TabPanel>
-    </TabPanels>
-  </Tabs>
-);
-
-const TemplateTopLine: Story = args => (
-  <Tabs {...args}>
-    <TabList linePosition="top">
+const Template: Story = ({
+  animation,
+  centered,
+  linePosition,
+  scrollable,
+  ...rest
+}) => (
+  <Tabs {...rest}>
+    <TabList
+      centered={centered}
+      linePosition={linePosition}
+      scrollable={scrollable}
+    >
       <Tab>Kafka</Tab>
       <Tab>Werther</Tab>
       <Tab>
@@ -77,7 +36,7 @@ const TemplateTopLine: Story = args => (
         <Badge>256</Badge>
       </Tab>
     </TabList>
-    <TabPanels>
+    <TabPanels animation={animation}>
       <TabPanel>
         <p>
           One morning, when Gregor Samsa woke from troubled dreams, he found
@@ -122,119 +81,19 @@ const TemplateTopLine: Story = args => (
   </Tabs>
 );
 
-const TemplateCentered: Story = args => (
-  <Tabs {...args} defaultIndex={1}>
-    <TabList centered>
-      <Tab>Kafka</Tab>
-      <Tab>Werther</Tab>
-      <Tab>
-        Cicero
-        <Badge>256</Badge>
-      </Tab>
-    </TabList>
-    <TabPanels>
-      <TabPanel>
-        <p>
-          One morning, when Gregor Samsa woke from troubled dreams, he found
-          himself transformed in his bed into a horrible vermin. He lay on his
-          armour-like back, and if he lifted his head a little he could see his
-          brown belly, slightly domed and divided by arches into stiff sections.
-          The bedding was hardly able to cover it and seemed ready to slide off
-          any moment. His many legs, pitifully thin compared with the size of
-          the rest of him, waved about helplessly as he looked. "What is
-          happened to me?" he thought. It was not a dream. His room, a proper
-          human room although a little too small, lay peacefully between its
-          four familiar walls.
-        </p>
-      </TabPanel>
-      <TabPanel>
-        <p>
-          A wonderful serenity has taken possession of my entire soul, like
-          these sweet mornings of spring which I enjoy with my whole heart. I am
-          alone, and feel the charm of existence in this spot, which was created
-          for the bliss of souls like mine. I am so happy, my dear friend, so
-          absorbed in the exquisite sense of mere tranquil existence, that I
-          neglect my talents. I should be incapable of drawing a single stroke
-          at the present moment; and yet I feel that I never was a greater
-          artist than now.
-        </p>
-      </TabPanel>
-      <TabPanel>
-        <p>
-          But I must explain to you how all this mistaken idea of denouncing
-          pleasure and praising pain was born and I will give you a complete
-          account of the system, and expound the actual teachings of the great
-          explorer of the truth, the master-builder of human happiness. No one
-          rejects, dislikes, or avoids pleasure itself, because it is pleasure,
-          but because those who do not know how to pursue pleasure rationally
-          encounter consequences that are extremely painful. Nor again is there
-          anyone who loves or pursues or desires to obtain pain of itself,
-          because it is pain, but because occasionally circumstances occur in
-          which toil and pain can procure him some great pleasure.
-        </p>
-      </TabPanel>
-    </TabPanels>
-  </Tabs>
-);
-
-const TemplateBordered: Story = args => (
-  <Tabs {...args}>
-    <TabList>
-      <Tab>Kafka</Tab>
-      <Tab>Werther</Tab>
-      <Tab>
-        Cicero
-        <Badge>256</Badge>
-      </Tab>
-    </TabList>
-    <TabPanels>
-      <TabPanel>
-        <p>
-          One morning, when Gregor Samsa woke from troubled dreams, he found
-          himself transformed in his bed into a horrible vermin. He lay on his
-          armour-like back, and if he lifted his head a little he could see his
-          brown belly, slightly domed and divided by arches into stiff sections.
-          The bedding was hardly able to cover it and seemed ready to slide off
-          any moment. His many legs, pitifully thin compared with the size of
-          the rest of him, waved about helplessly as he looked. "What is
-          happened to me?" he thought. It was not a dream. His room, a proper
-          human room although a little too small, lay peacefully between its
-          four familiar walls.
-        </p>
-      </TabPanel>
-      <TabPanel>
-        <p>
-          A wonderful serenity has taken possession of my entire soul, like
-          these sweet mornings of spring which I enjoy with my whole heart. I am
-          alone, and feel the charm of existence in this spot, which was created
-          for the bliss of souls like mine. I am so happy, my dear friend, so
-          absorbed in the exquisite sense of mere tranquil existence, that I
-          neglect my talents. I should be incapable of drawing a single stroke
-          at the present moment; and yet I feel that I never was a greater
-          artist than now.
-        </p>
-      </TabPanel>
-      <TabPanel>
-        <p>
-          But I must explain to you how all this mistaken idea of denouncing
-          pleasure and praising pain was born and I will give you a complete
-          account of the system, and expound the actual teachings of the great
-          explorer of the truth, the master-builder of human happiness. No one
-          rejects, dislikes, or avoids pleasure itself, because it is pleasure,
-          but because those who do not know how to pursue pleasure rationally
-          encounter consequences that are extremely painful. Nor again is there
-          anyone who loves or pursues or desires to obtain pain of itself,
-          because it is pain, but because occasionally circumstances occur in
-          which toil and pain can procure him some great pleasure.
-        </p>
-      </TabPanel>
-    </TabPanels>
-  </Tabs>
-);
-
-const TemplateScrollable: Story = args => (
-  <Tabs {...args}>
-    <TabList scrollable>
+const TemplateScrollable: Story = ({
+  animation,
+  centered,
+  linePosition,
+  scrollable,
+  ...rest
+}) => (
+  <Tabs {...rest}>
+    <TabList
+      centered={centered}
+      linePosition={linePosition}
+      scrollable={scrollable}
+    >
       <Tab>Kafka</Tab>
       <Tab>Werther</Tab>
       <Tab>
@@ -248,7 +107,7 @@ const TemplateScrollable: Story = args => (
       <Tab>Kafka the longer</Tab>
       <Tab>Werther the longer than longer</Tab>
     </TabList>
-    <TabPanels>
+    <TabPanels animation={animation}>
       <TabPanel>
         <p>
           One morning, when Gregor Samsa woke from troubled dreams, he found
@@ -289,122 +148,15 @@ const TemplateScrollable: Story = args => (
           which toil and pain can procure him some great pleasure.
         </p>
       </TabPanel>
+      <TabPanel>Long Long Cicero</TabPanel>
+      <TabPanel>Kafka the longer</TabPanel>
+      <TabPanel>Werther the longer than longer</TabPanel>
     </TabPanels>
   </Tabs>
 );
 
-const TemplateFadeAnimation: Story = args => (
-  <Tabs {...args}>
-    <TabList>
-      <Tab>Kafka</Tab>
-      <Tab>Werther</Tab>
-      <Tab>
-        Cicero
-        <Badge>256</Badge>
-      </Tab>
-    </TabList>
-    <TabPanels animation="fade">
-      <TabPanel>
-        <p>
-          One morning, when Gregor Samsa woke from troubled dreams, he found
-          himself transformed in his bed into a horrible vermin. He lay on his
-          armour-like back, and if he lifted his head a little he could see his
-          brown belly, slightly domed and divided by arches into stiff sections.
-          The bedding was hardly able to cover it and seemed ready to slide off
-          any moment. His many legs, pitifully thin compared with the size of
-          the rest of him, waved about helplessly as he looked. "What is
-          happened to me?" he thought. It was not a dream. His room, a proper
-          human room although a little too small, lay peacefully between its
-          four familiar walls.
-        </p>
-      </TabPanel>
-      <TabPanel>
-        <p>
-          A wonderful serenity has taken possession of my entire soul, like
-          these sweet mornings of spring which I enjoy with my whole heart. I am
-          alone, and feel the charm of existence in this spot, which was created
-          for the bliss of souls like mine. I am so happy, my dear friend, so
-          absorbed in the exquisite sense of mere tranquil existence, that I
-          neglect my talents. I should be incapable of drawing a single stroke
-          at the present moment; and yet I feel that I never was a greater
-          artist than now.
-        </p>
-      </TabPanel>
-      <TabPanel>
-        <p>
-          But I must explain to you how all this mistaken idea of denouncing
-          pleasure and praising pain was born and I will give you a complete
-          account of the system, and expound the actual teachings of the great
-          explorer of the truth, the master-builder of human happiness. No one
-          rejects, dislikes, or avoids pleasure itself, because it is pleasure,
-          but because those who do not know how to pursue pleasure rationally
-          encounter consequences that are extremely painful. Nor again is there
-          anyone who loves or pursues or desires to obtain pain of itself,
-          because it is pain, but because occasionally circumstances occur in
-          which toil and pain can procure him some great pleasure.
-        </p>
-      </TabPanel>
-    </TabPanels>
-  </Tabs>
-);
-
-const TemplateNoAnimation: Story = args => (
-  <Tabs {...args}>
-    <TabList>
-      <Tab>Kafka</Tab>
-      <Tab>Werther</Tab>
-      <Tab>
-        Cicero
-        <Badge>256</Badge>
-      </Tab>
-    </TabList>
-    <TabPanels animation={false}>
-      <TabPanel>
-        <p>
-          One morning, when Gregor Samsa woke from troubled dreams, he found
-          himself transformed in his bed into a horrible vermin. He lay on his
-          armour-like back, and if he lifted his head a little he could see his
-          brown belly, slightly domed and divided by arches into stiff sections.
-          The bedding was hardly able to cover it and seemed ready to slide off
-          any moment. His many legs, pitifully thin compared with the size of
-          the rest of him, waved about helplessly as he looked. "What is
-          happened to me?" he thought. It was not a dream. His room, a proper
-          human room although a little too small, lay peacefully between its
-          four familiar walls.
-        </p>
-      </TabPanel>
-      <TabPanel>
-        <p>
-          A wonderful serenity has taken possession of my entire soul, like
-          these sweet mornings of spring which I enjoy with my whole heart. I am
-          alone, and feel the charm of existence in this spot, which was created
-          for the bliss of souls like mine. I am so happy, my dear friend, so
-          absorbed in the exquisite sense of mere tranquil existence, that I
-          neglect my talents. I should be incapable of drawing a single stroke
-          at the present moment; and yet I feel that I never was a greater
-          artist than now.
-        </p>
-      </TabPanel>
-      <TabPanel>
-        <p>
-          But I must explain to you how all this mistaken idea of denouncing
-          pleasure and praising pain was born and I will give you a complete
-          account of the system, and expound the actual teachings of the great
-          explorer of the truth, the master-builder of human happiness. No one
-          rejects, dislikes, or avoids pleasure itself, because it is pleasure,
-          but because those who do not know how to pursue pleasure rationally
-          encounter consequences that are extremely painful. Nor again is there
-          anyone who loves or pursues or desires to obtain pain of itself,
-          because it is pain, but because occasionally circumstances occur in
-          which toil and pain can procure him some great pleasure.
-        </p>
-      </TabPanel>
-    </TabPanels>
-  </Tabs>
-);
-
-const TemplateCustomized: Story = args => (
-  <Tabs {...args} size="sm" defaultIndex={2}>
+const TemplateCustomized: Story = ({ showAnimation, ...rest }) => (
+  <Tabs {...rest} size="sm" defaultIndex={2}>
     <TabList theme="yellow">
       <Tab>Kafka</Tab>
       <Tab>Werther</Tab>
@@ -413,7 +165,7 @@ const TemplateCustomized: Story = args => (
         <Badge theme="orange-light">256</Badge>
       </Tab>
     </TabList>
-    <TabPanels theme="yellow-light" animation="fade">
+    <TabPanels theme="yellow-light" showAnimation={showAnimation}>
       <TabPanel size="xs">
         <p>
           One morning, when Gregor Samsa woke from troubled dreams, he found
@@ -459,10 +211,33 @@ const TemplateCustomized: Story = args => (
 );
 
 export const Default = Template.bind({});
-export const TopLine = TemplateTopLine.bind({});
-export const Centered = TemplateCentered.bind({});
-export const Bordered = TemplateBordered.bind({});
+export const TopLine = Template.bind({});
+export const Centered = Template.bind({});
+export const Bordered = Template.bind({});
 export const Scrollable = TemplateScrollable.bind({});
-export const FadeAnimation = TemplateFadeAnimation.bind({});
-export const NoAnimation = TemplateNoAnimation.bind({});
+export const Animation = Template.bind({});
 export const Customized = TemplateCustomized.bind({});
+
+TopLine.args = {
+  linePosition: 'top',
+};
+
+Centered.args = {
+  centered: true,
+};
+
+Bordered.args = {
+  bordered: true,
+};
+
+Scrollable.args = {
+  scrollable: true,
+};
+
+Animation.args = {
+  animation: 'fade',
+};
+
+Customized.args = {
+  showAnimation: 'flatify-dropdown-show 0.3s',
+};
