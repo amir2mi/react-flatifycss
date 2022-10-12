e08e5bf9dfd3524e1a2f330124c631c5123de586
Amir M. Mohamadi <mail.amir2mi@gmail.com>
2022-10-13 01:57:02 +0330

    Added storybook doc addons for MDX doc pages

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,15 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.@(ts|tsx|js|jsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  stories: [
+    '../stories/**/*.stories.@(ts|tsx|js|jsx)',
+    '../stories/**/*.stories.@(mdx)',
+  ],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-docs',
+  ],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true, // type-check stories during Storybook build
-  }
+  },
 };
